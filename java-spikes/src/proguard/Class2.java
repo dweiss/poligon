75a85faa1cbaa1e4adf5f3f367831be71c5a6346
dweiss <dawid.weiss@gmail.com>
2010-04-16 10:03:55 +0200

    Added selective proguard obfuscation test.'

---
+++ java-spikes/src/proguard/Class2.java
@@ -0,0 +1,19 @@
+package proguard;
+
+@SuppressWarnings("unused")
+public class Class2
+{
+    public static void method_public_static() {}
+    public void method_public()  {}
+    private static void method_private_static() {}
+    private void method_private() {}
+    static void method_package_scope_static() {}
+    void method_package_scope() {}
+    
+    public static void prefix_public_static() {}
+    public void prefix_public()  {}
+    private static void prefix_private_static() {}
+    private void prefix_private() {}
+    static void prefix_package_scope_static() {}
+    void prefix_package_scope() {}
+}
