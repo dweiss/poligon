526456683366b8f46dbe328b264997998b2094c1
Dawid Weiss <dawid.weiss@carrotsearch.com>
2015-12-11 15:26:51 +0100

    SVN dump renumber.

---
+++ svnrdump-renumber/src/main/java/Renumber.java
@@ -0,0 +1,153 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.function.Consumer;
+
+import com.carrotsearch.hppc.ByteArrayList;
+import com.carrotsearch.hppc.LongLongHashMap;
+import com.google.common.io.ByteStreams;
+
+public class Renumber {
+  public static void main(String[] args) throws Exception {
+    ArrayList<Path> names = new ArrayList<>();
+    Path revsDir = Paths.get("d:\\data\\lucene-svn\\revs");
+    Files.walkFileTree(revsDir, new SimpleFileVisitor<Path>() {
+      @Override
+      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (file.getFileName().toString().endsWith(".rev")) {
+          names.add(file);
+        }
+        return FileVisitResult.CONTINUE;
+      }
+    });
+
+    // Read all revisions.
+    LongLongHashMap revisions = new LongLongHashMap();
+    int cnt = 0;
+    for (Path p : names) {
+      if ((++cnt % 10000) == 0) {
+        System.out.println("1> " + cnt);
+      }
+      visitDumpFile(p,
+          (headers) -> {
+            String revNum = headers.get("Revision-number");
+            if (revNum != null) {
+              revisions.put(Long.parseLong(revNum), 0);
+              String revName = "r" + revNum + ".rev";
+              if (!p.getFileName().toString().equals(revName)) {
+                throw new RuntimeException("Expected identical rev. numbers and files: "
+                    + p.getFileName() + " " + revName);
+              }
+            }
+          },
+          (body) -> {});
+    }
+
+    // Remap revisions to be contiguous and start at 1.
+    long [] originalRevs = Arrays.stream(revisions.keys().toArray()).sorted().toArray();
+    
+    long rev = 1;
+    for (long key : originalRevs) {
+      revisions.put(key, rev++);
+    }
+
+    cnt = 0;
+    try (OutputStream os = new BufferedOutputStream(Files.newOutputStream(revsDir.resolveSibling("renamed.dump")))) {
+      for (long origRev : originalRevs) {
+        long newRev = revisions.getOrDefault(origRev, -1);
+        if (newRev < 0) throw new RuntimeException();
+  
+        if ((++cnt % 10000) == 0) {
+          System.out.println("2> " + cnt);
+        }
+  
+        Path p = revsDir.resolve("r" + origRev + ".rev");
+        visitDumpFile(p,
+            (headers) -> {
+              // TODO: convert headers, emit once:
+              // SVN-fs-dump-format-version: 3
+              // UUID: 13f79535-47bb-0310-9956-ffa450edef68
+              // emit bodies.
+
+              String revNum = headers.get("Node-copyfrom-rev");
+              if (revNum != null) {
+                long parseLong = Long.parseLong(revNum);
+                if (!revisions.containsKey(parseLong)) {
+                  throw new RuntimeException();
+                }
+              }
+            },
+            (body) -> {});
+      }
+    }
+
+    System.out.println("Revisions: " + revisions.size());
+  }
+
+  private static void visitDumpFile(Path path, 
+                                    Consumer<LinkedHashMap<String, String>> headerConsumer,
+                                    Consumer<InputStream> bodyConsumer) throws Exception {
+    LinkedHashMap<String, String> headers = new LinkedHashMap<>();
+    ByteArrayList lineBuffer = new ByteArrayList();
+    try (InputStream is = new BufferedInputStream(Files.newInputStream(path))) {
+      // read headers
+      do {
+        headers(is, headers, lineBuffer);
+        if (headers.isEmpty()) {
+          break;
+        }
+        
+        headerConsumer.accept(headers);
+
+        if (headers.containsKey("Content-length")) {
+          long length = Long.parseLong(headers.get("Content-length"));
+          InputStream limit = ByteStreams.limit(is, length);
+          bodyConsumer.accept(limit);
+          limit.skip(length);
+        }
+      } while (true);
+    }
+  }
+
+  private static void headers(InputStream is, LinkedHashMap<String, String> headers, ByteArrayList lineBuffer) 
+      throws IOException {
+    headers.clear();
+    while (true) {
+      lineBuffer.clear();
+      int b;
+      while ((b = is.read()) >= 0) {
+        if (b == '\n') { 
+          break;
+        }
+        lineBuffer.add((byte) b);
+      }
+
+      if (lineBuffer.isEmpty()) {
+        return;
+      }
+      
+      String headerLine = new String(lineBuffer.buffer, 0, lineBuffer.size(), StandardCharsets.US_ASCII);
+      int sep = headerLine.indexOf(": ");
+      if (sep < 0) throw new IOException("No separator: " + headerLine);
+
+      String key = headerLine.substring(0, sep);
+      String value = headerLine.substring(sep + 2);
+      if (headers.containsKey(key)) {
+        throw new IOException("Dup. key " + key + ":\n 1: " + value + "\n 2: " + headers.get(key));
+      }
+      headers.put(key, value);
+    }
+  }
+}
